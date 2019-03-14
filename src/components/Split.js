9f4907ebd61297fe426e0d00226aedd2ede73f5a
jkearle <jeremy@jeremyearle.net>
2019-03-13 19:57:26 -0500

    Add a stats component to display query stats when they are ran

--- src/components/Split.js
+++ src/components/Split.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Split.css';
 import Query from "./Query";
 import ResultTable from "./ResultTable";
+import Stats from "./Stats";
 
 class Split extends Component {
     render() {
@@ -9,14 +10,17 @@ class Split extends Component {
         if (!this.props.split) {
             gridstate += " SplitGridSingle";
         }
+        const took = 0;
         return (
             <div className="Split">
                 <div className={gridstate}>
                     <div className="SplitQuery1">
                         <Query query={this.props.query1} onClick={() => this.props.queryClick(1)}/>
+                        <Stats took={took}/>
                     </div>
                     <div className="SplitQuery2">
                         <Query query={this.props.query2} onClick={() => this.props.queryClick(2)}/>
+                        <Stats took={this.props.took2}/>
                     </div>
                     <div className="SplitResults1">
                         <ResultTable results={this.props.results1}/>
