4df9810c5392d676f12758c02019e66099758659
Chris Cornelius <ccornelius@dealnews.com>
2019-04-30 14:38:27 -0500

    Found some extra props no longer needed.

--- src/components/Split.js
+++ src/components/Split.js
@@ -5,14 +5,9 @@ import {SplitQuery, SplitResults} from "../components";
 
 class Split extends Component {
     render() {
-        const {queryClick} = this.props;
-
         return (
             <div className="Split">
-                <SplitQuery split={this.props.split}
-                            query1={this.props.query1}
-                            query2={this.props.query2}
-                            queryClick={queryClick}/>
+                <SplitQuery/>
                 <SplitResults/>
             </div>
         );
